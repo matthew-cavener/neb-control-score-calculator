74e4aa3d5351eefa02d3ca542e52c9ca689310a3
Matthew Cavener <matthew.cavener@gmail.com>
2025-03-04 23:16:11 -0800

    sorta working ish?

--- godot/scenes/main.gd
+++ godot/scenes/main.gd
@@ -1,9 +1,28 @@
 extends Node2D
 
+
 const SCORE_TIMER_INTERVAL = 10.0
 const SCORE_PER_CAP_POINT = 2
+const CAPTURE_POINTS = 5
+
 
 var score_timer: Timer
+var projected_loser: String = ""
+var loser_caps_needed: int = 0
+var time_to_red_victory: float = 999999
+var time_to_blue_victory: float = 999999
+var red_times_to_win: Dictionary = {}
+var blue_times_to_win: Dictionary = {}
+var red_time_to_win: float = 0
+var blue_time_to_win: float = 0
+var red_caps_needed: int = 0
+var blue_caps_needed: int = 0
+var approx_time_to_extra_cap_needed: String = ""
+
+
+@onready var points_to_red_victory = $ScoreLimit.text
+@onready var points_to_blue_victory = $ScoreLimit.text
+
 
 func _ready() -> void:
     $BlueTeamScore.text = str(0)
@@ -15,19 +34,14 @@ func _ready() -> void:
     add_child(score_timer)
     score_timer.start()
 
-func _process(delta: float) -> void:
-    pass
-
-func _on_score_timer_timeout() -> void:
-    calculate_score()
 
 func calculate_score() -> void:
-    print("score_timer timeout, calculating score")
     var score_limit = float($ScoreLimit.text)
     var red_caps = get_tree().get_nodes_in_group("red_point")
     var blue_caps = get_tree().get_nodes_in_group("blue_point")
-    var red_score = float($RedTeamScore.text)
-    var blue_score = float($BlueTeamScore.text)
+    var neutral_caps = get_tree().get_nodes_in_group("neutral_point")
+    var red_score = int($RedTeamScore.text)
+    var blue_score = int($BlueTeamScore.text)
     red_score += SCORE_PER_CAP_POINT * red_caps.size()
     blue_score += SCORE_PER_CAP_POINT * blue_caps.size()
 
@@ -37,45 +51,82 @@ func calculate_score() -> void:
     $BlueTeamScore.text = str(blue_score)
     $ProgressBarBlue.texture_bar_fill_fraction = blue_score / score_limit
     
-    var points_to_red_victory = score_limit - red_score
-    var points_to_blue_victory = score_limit - blue_score
-    var time_to_red_victory = (points_to_red_victory * SCORE_TIMER_INTERVAL) / (red_caps.size() * SCORE_PER_CAP_POINT)
-    var time_to_blue_victory = (points_to_blue_victory * SCORE_TIMER_INTERVAL) / (blue_caps.size() * SCORE_PER_CAP_POINT)
+    points_to_red_victory = score_limit - red_score
+    points_to_blue_victory = score_limit - blue_score
 
-    var red_times_to_win = {}
-    var blue_times_to_win = {}
-
-    for number_of_caps in range(1, 6):
-        var red_time_to_win = (points_to_red_victory * SCORE_TIMER_INTERVAL) / (number_of_caps * SCORE_PER_CAP_POINT)
-        var blue_time_to_win = (points_to_blue_victory * SCORE_TIMER_INTERVAL) / (number_of_caps * SCORE_PER_CAP_POINT)
+    for number_of_caps in range(1, CAPTURE_POINTS + 1):
+        red_time_to_win = calculate_time_to_win(points_to_red_victory, number_of_caps)
+        blue_time_to_win = calculate_time_to_win(points_to_blue_victory, number_of_caps)
         red_times_to_win[number_of_caps] = (red_time_to_win)
         blue_times_to_win[number_of_caps] = (blue_time_to_win)
 
-    var projected_loser = ""
+    if red_caps.size() > 0:
+        time_to_red_victory = red_times_to_win[red_caps.size()]
+    else:
+        time_to_red_victory = 999999
+
+    if blue_caps.size() > 0:
+        time_to_blue_victory = blue_times_to_win[blue_caps.size()]
+    else:
+        time_to_blue_victory = 999999
 
     if time_to_red_victory > time_to_blue_victory:
         projected_loser = "Red"
+        loser_caps_needed = red_caps_needed
     else:
         projected_loser = "Blue"
-
-    var red_caps_needed = "game should be over"
-    var blue_caps_needed = "game should be over"
+        loser_caps_needed = blue_caps_needed
 
     for key in red_times_to_win.keys():
-        if blue_times_to_win.has(5 - key) and red_times_to_win[key] < blue_times_to_win[5 - key]:
+        if blue_times_to_win.has(CAPTURE_POINTS - key) and red_times_to_win[key] < blue_times_to_win[CAPTURE_POINTS - key]:
             red_caps_needed = key
             break
     for key in blue_times_to_win.keys():
-        if red_times_to_win.has(5 - key) and blue_times_to_win[key] < red_times_to_win[5 - key]:
+        if red_times_to_win.has(CAPTURE_POINTS - key) and blue_times_to_win[key] < red_times_to_win[CAPTURE_POINTS - key]:
             blue_caps_needed = key
             break
+
+    approx_time_to_extra_cap_needed = convert_seconds_to_minutes_seconds(calculate_time_before_extra_cap_needed(projected_loser, red_times_to_win, blue_times_to_win, red_caps_needed, blue_caps_needed, red_caps.size(), blue_caps.size()))
+
+    for key in red_times_to_win.keys():
+        red_times_to_win[key] = convert_seconds_to_minutes_seconds(int(red_times_to_win[key]))
+    for key in blue_times_to_win.keys():
+        blue_times_to_win[key] = convert_seconds_to_minutes_seconds(int(blue_times_to_win[key]))
+
+    # var red_team_extras = "Red team caps:time to win: " + str(red_times_to_win)
+    # var blue_team_extras = "Blue team caps:time to win: " + str(blue_times_to_win)
 
     var red_team_victory_conditions = "Red team needs " + str(points_to_red_victory) + " points to win in " + convert_seconds_to_minutes_seconds(int(time_to_red_victory))
     var blue_team_victory_conditions = "Blue team needs " + str(points_to_blue_victory) + " points to win in " + convert_seconds_to_minutes_seconds(int(time_to_blue_victory))
 
     red_team_victory_conditions += "\nRed team needs " + str(red_caps_needed) + " caps to win.\n"
     blue_team_victory_conditions += "\nBlue team needs " + str(blue_caps_needed) + " caps to win.\n"
-    $VictoryConditionsLabel.text = red_team_victory_conditions  + "\n" +  blue_team_victory_conditions + "\n" + projected_loser + " needs to make something happen."
+    $VictoryConditionsLabel.text = (
+        red_team_victory_conditions +
+        "\n" +
+        blue_team_victory_conditions +
+        "\n" +
+        projected_loser + " needs to make something happen. They will need " + str(loser_caps_needed + 1) + " caps to win in " + approx_time_to_extra_cap_needed
+    )
+
+func calculate_time_to_win(points_to_victory: int, caps: int) -> float:
+    return (points_to_victory * SCORE_TIMER_INTERVAL) / (caps * SCORE_PER_CAP_POINT)
+
+
+func calculate_time_before_extra_cap_needed(
+    projected_loser: String,
+    red_times_to_win: Dictionary,
+    blue_times_to_win: Dictionary,
+    red_caps_needed: int,
+    blue_caps_needed: int,
+    red_caps_size: int,
+    blue_caps_size: int
+    ) -> float:
+    if projected_loser == "Red":
+        return (blue_times_to_win[CAPTURE_POINTS - red_caps_needed] - red_times_to_win[red_caps_needed]) / ((SCORE_PER_CAP_POINT * 0.8) / red_caps_size)
+    else:
+        return (red_times_to_win[CAPTURE_POINTS - blue_caps_needed] - blue_times_to_win[blue_caps_needed]) / ((SCORE_PER_CAP_POINT * 0.8) / blue_caps_size)
+
 
 func convert_seconds_to_minutes_seconds(seconds: int) -> String:
     var minutes = seconds / 60
@@ -83,6 +134,19 @@ func convert_seconds_to_minutes_seconds(seconds: int) -> String:
     return str(minutes).pad_zeros(2) + ":" + str(remaining_seconds).pad_zeros(2)
 
 
-func _on_timer_reset_button_pressed() -> void:
-    print("timer reset button pressed")
+func _on_score_timer_timeout() -> void:
+    print("score_timer timeout, calculating score")
+    calculate_score()
+
+
+func _on_sync_timer_tick_score_button_pressed() -> void:
+    calculate_score()
     score_timer.start()
+
+
+func _on_timer_reset_button_pressed() -> void:
+    score_timer.start()
+
+
+func _on_manual_score_tick_button_pressed() -> void:
+    calculate_score()
